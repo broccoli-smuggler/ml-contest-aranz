5f8c31d759aa5e108668f962235a10bd3066c443
Joshua Jordan <joshua.jordan@ho.aranzgeo.com>
2017-01-10 16:55:24 +1300

    Removed 2nd run

--- hypter.py
+++ hypter.py
@@ -37,30 +37,11 @@ train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-############################################## 2
-labels_T2 = tf.convert_to_tensor(hot_vals[:dev_sample_index])
-test_labels_T2 = tf.convert_to_tensor(hot_vals[dev_sample_index:])
-
-y_2 = tf.placeholder(tf.float32, shape=[None, NUM_FACIES])
-
-# network setup
-y2, x2, features_T2, test_features_T2 = convolutional_network(all_data, dev_sample_index)
-
-# loss function used to train
-cross_entropy2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y2, y_2))
-
-# backprop
-train_step2 = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy2)
-
-# Accuracy calculations
-correct_prediction2 = tf.equal(tf.argmax(y2, 1), tf.argmax(y_2, 1))
-accuracy2 = tf.reduce_mean(tf.cast(correct_prediction2, tf.float32))
-
 # session init
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for i in range(300000):
+for i in range(20000):
     x_vals, y_labels, x_vals_t, y_labels_t = sess.run([features_T, labels_T, test_features_T, test_labels_T])
 
     train_data = {x: x_vals, y_: y_labels}
@@ -69,20 +50,10 @@ for i in range(300000):
     test_data = {x: x_vals_t, y_: y_labels_t}
     test_acc = sess.run(accuracy, feed_dict=test_data)
 
-    x_vals2, y_labels2, x_vals_t2, y_labels_t2 = sess.run([features_T2, labels_T2, test_features_T2, test_labels_T2])
-
-    train_data2 = {x2: x_vals2, y_2: y_labels2}
-    _, train_acc2 = sess.run([train_step2, accuracy2], feed_dict=train_data2)
-
-    test_data2 = {x2: x_vals_t2, y_2: y_labels_t2}
-    test_acc2 = sess.run(accuracy2, feed_dict=test_data2)
-
     if i % 1000 == 0:
         print('epoch', i / 1000)
         print('test acc', test_acc)
         print('train acc', train_acc, '\n')
-        print('test acc d', test_acc2)
-        print('train acc d', train_acc2, '\n')
 
 print('test acc final', test_acc)
 print('train acc final', sess.run(accuracy, feed_dict=train_data), '\n')
