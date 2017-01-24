9643321ced5c7cf0343e5346d89b3fd8abaf7dfd
Joshua Jordan <joshua.jordan@ho.aranzgeo.com>
2017-01-24 15:30:18 +1300

    Introduced random seeding back in, also implemented stacking

--- networks_setups.py
+++ networks_setups.py
@@ -4,14 +4,29 @@ import numpy as np
 NUM_FACIES = 9
 NUM_INPUTS = 9
 
+forms_replace = {'A1 LM': 1,
+                 'A1 SH': 2,
+                 'B1 LM': 3,
+                 'B1 SH': 4,
+                 'B2 LM': 5,
+                 'B2 SH': 6,
+                 'B3 LM': 7,
+                 'B3 SH': 7,
+                 'B4 LM': 8,
+                 'B4 SH': 9,
+                 'B5 LM': 10,
+                 'B5 SH': 11,
+                 'C LM': 12,
+                 'C SH': 13}
 
-def add_input_noise_from_facies(data, adjacent_facies, noise_pecentage=0.05):
+
+def add_input_noise_from_facies(data, adjacent_facies, train_indices, noise_pecentage=0.05, seed=1):
     noise_dict = {i: k for i, k in enumerate(adjacent_facies)}
 
-    vals = data['Facies'].values
+    vals = data['Facies'].values[train_indices]
     vals -= 1
 
-    np.random.seed(8)
+    np.random.seed(seed)
     random_index = np.random.permutation(np.arange(int(len(vals) * noise_pecentage)))
     to_randomise = vals[random_index]
     randomed = [np.random.choice(noise_dict[arr]) for arr in to_randomise]
@@ -20,7 +35,6 @@ def add_input_noise_from_facies(data, adjacent_facies, noise_pecentage=0.05):
     hot_vals = np.zeros((vals.size, vals.max() + 1))
     hot_vals[np.arange(vals.size), vals] = 1
     data = data.drop('Facies', axis=1)
-
     return hot_vals, data
 
 
@@ -30,9 +44,7 @@ def cleanup_csv(data, standardize=True):
     data = data.drop('Well Name', axis=1)
 
     # Replace formation and well names with corresponding integers
-    forms = set(data['Formation'])
-    form_replace = {f: i for f, i in zip(forms, np.arange(len(forms)))}
-    data['Formation'] = data['Formation'].replace(form_replace)
+    data['Formation'] = data['Formation'].replace(forms_replace)
 
     # Fill missing values and normalize
     data['PE'] = data['PE'].fillna(value=0)
@@ -77,11 +89,11 @@ def two_layer_network(all_data, sample_index, dropout=False):
     return l_y, l_x, features_T, test_features_T
 
 
-def three_layer_network(all_data, sample_index, seed=None, dropout=False):
+def three_layer_network(train_data, test_data, seed=None, dropout=False):
     # setup
     l_x = tf.placeholder(tf.float32, shape=[None, NUM_INPUTS])
-    features_T = tf.pack(all_data.values[:sample_index])
-    test_features_T = tf.pack(all_data.values[sample_index:])
+    features_T = tf.pack(train_data)
+    test_features_T = tf.pack(test_data)
 
     # layer sizes
     k = 100
@@ -104,8 +116,8 @@ def three_layer_network(all_data, sample_index, seed=None, dropout=False):
         y1d = tf.nn.relu(tf.matmul(l_x, w1) + b1)
         y1 = tf.nn.dropout(y1d, 0.9)
         y2d = tf.nn.relu(tf.matmul(y1, w2) + b2)
-        y2 = tf.nn.dropout(y2d, 0.95)
-        y3d = tf.nn.relu(tf.matmul(y2, w3) + b3)
+        # y2 = tf.nn.dropout(y2d, 0.95)
+        y3d = tf.nn.relu(tf.matmul(y2d, w3) + b3)
         y3 = tf.nn.dropout(y3d, 0.95)
     else:
         y1 = tf.nn.relu(tf.matmul(l_x, w1) + b1)
